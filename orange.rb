b4a6ddcc11fd07da12ebcd09b3e418c1bb4e2155
Vincent Cabansag <vcabansag@gmail.com>
2013-11-04 08:50:49 -0600

    Added orange tree challenge.

---
+++ orange.rb
@@ -0,0 +1,25 @@
+# Orange Tree Challenge
+
+# Let's create an orange tree that grows and lives for a finite time.
+# It will bear and drop fruit. And you'll be able to harvest it.
+
+# Start off by creating an object class called OrangeTree that has
+# a height method that returns it's height and one_year_passes method,
+# when called, ages the tree by one year.
+
+# Each year your tree grows, any amount you choose, and, after some time,
+# your tree should die. Poor tree.
+
+# Once you can demonstrate the behavior of creating a tree, letting it
+# grow age, and die, then you should give it the ability to bear fruit.
+# Your tree shouldn't bear any fruit until it's reached a couple years
+# of maturity. If you're up to it, your tree should produce more oranges
+# as it ages, and it tapers-off just before it dies--totally optional.
+
+# Finally, you should be able to count and harvest oranges each year.
+# Create a method called count_the_oranges that returns the number of
+# oranges on the tree and create a method called pick_an_orange that
+# harvests one orange. Make sure that any fruit that's not harvested
+# at the end of the year drops it's fruit. Demonstrate this behavior
+# of counting, harvesting and the dropping of oranges in your program.
+
